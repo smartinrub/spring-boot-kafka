b3952cb5879c3b73176049c30ba7e6a373fec1ae
Sergio Martin Rubio <econsergio@gmail.com>
2019-10-15 00:13:23 +0100

    create kafka example with spring boot autoconfiguration

--- src/main/java/com/sergiomartinrubio/springbootkafka/SpringBootKafkaApplication.java
+++ src/main/java/com/sergiomartinrubio/springbootkafka/SpringBootKafkaApplication.java
@@ -2,17 +2,26 @@ package com.sergiomartinrubio.springbootkafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @Slf4j
 @SpringBootApplication
-public class SpringBootKafkaApplication {
+public class SpringBootKafkaApplication implements CommandLineRunner {
+
+    @Autowired
+    KafkaTemplate<String, String> template;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootKafkaApplication.class, args);
     }
 
-
+    @Override
+    public void run(String... args) {
+        template.send("firstTopic", "0", "Hello World");
+        template.send("firstTopic", "1","Goodbye");
+        log.info("Done");
+    }
 }
