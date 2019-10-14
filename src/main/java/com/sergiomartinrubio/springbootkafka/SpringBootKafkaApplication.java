d36b5fd78d14b85e804a5ef3017311e4b2e500ca
smartinrubio <smartinrubio@hotels.com>
2019-10-14 17:24:21 +0100

    add initial files

---
+++ src/main/java/com/sergiomartinrubio/springbootkafka/SpringBootKafkaApplication.java
@@ -0,0 +1,20 @@
+package com.sergiomartinrubio.springbootkafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Slf4j
+@SpringBootApplication
+public class SpringBootKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaApplication.class, args);
+    }
+
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+}
